0751bdc809c55dcaed3f0508be8645468b074377
Justice <122542069+goob8079@users.noreply.github.com>
2025-09-28 18:40:51 +0900

    ship class and tests created gameboard class started

---
+++ ship.js
@@ -0,0 +1,25 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        if (this.hits === this.length) {
+            return;
+        }
+        
+        this.hits += 1;
+    }
+    
+    isSunk() {
+        if (this.hits === this.length) {
+            this.sunk = true;
+        }
+        
+        return this.sunk;
+    }
+}
+
+module.exports = { Ship };
