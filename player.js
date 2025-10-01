a0bcc9f363d95e464b31258eec117842a23c6165
Justice <122542069+goob8079@users.noreply.github.com>
2025-10-01 20:05:51 +0900

    created player.js

---
+++ player.js
@@ -0,0 +1,9 @@
+import { Gameboard } from "./gameboard";
+
+class Player{
+    constructor(name) {
+        this.name = name;
+        this.gameboard = new Gameboard;
+        this.cpu = false;
+    }
+}
